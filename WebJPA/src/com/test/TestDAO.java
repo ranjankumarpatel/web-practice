013f2d014cc06b33044627d231c9b71b45c233c7
ranjankumarpatel <ranjanpatelbbsr@gmail.com>
2014-08-24 00:18:30 +0530

    ranjan

---
+++ WebJPA/src/com/test/TestDAO.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class TestDAO {
+
+	public TestDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("ranjan");
+
+	}
+
+}
